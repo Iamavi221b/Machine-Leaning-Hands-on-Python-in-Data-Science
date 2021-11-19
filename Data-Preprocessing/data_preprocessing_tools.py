ea1f8995f84d398df3d8546f19c1751558f06681
Iamavi221b <iamavi1012@gmail.com>
2021-11-19 21:49:48 +0530

    added dataset x and y

--- Data-Preprocessing/data_preprocessing_tools.py
+++ Data-Preprocessing/data_preprocessing_tools.py
@@ -5,7 +5,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import time
+start_time = time.time()
 
 ## Importing the dataset
 
 dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+print(X, y)
+
+print("----- %s seconds -----"% (time.time()-start_time))
